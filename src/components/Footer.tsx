c68c042fd760f0465ad2aeef0e7630e75680ba64
kobenguyent <kobenguyent@gmail.com>
2024-02-02 09:46:20 +0100

    try out bunjs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return(
     <footer className="text-muted text-center" style={{ marginTop: 10}}>
-      <small>(c) { new Date().getFullYear() } - 🏀 Kobe Nguyen 🏀 with ❤️ </small>
+      <small>(c) { new Date().getFullYear() } - Built by 🏀 KobeT 🏀 with ❤️ </small>
     </footer>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), istanbul({
+    include: 'src/*',
+    exclude: ['node_modules', 'test/'],
+    extension: [ '.js', '.ts', '.vue' ],
+    requireEnv: true,
+  }),],
   base: '/qa-utils/',
   assetsInclude: ['**/*.png', '**/*.jpeg', '**/*.svg'],
   build: {
